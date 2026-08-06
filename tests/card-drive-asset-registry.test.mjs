b5a6559b41f1cbe709c64fc316b691715b37efe6
Sean Lin <gshan1209@gmail.com>
2026-08-06 23:06:12 +0800

    test: import Node URL for Drive schema contracts

--- tests/card-drive-asset-registry.test.mjs
+++ tests/card-drive-asset-registry.test.mjs
@@ -1,6 +1,7 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
+import { URL } from 'node:url';
 
 import {
   DRIVE_ASSET_STATUSES,
