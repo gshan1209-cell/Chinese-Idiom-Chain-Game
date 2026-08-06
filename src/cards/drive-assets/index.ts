cbaae50df31e3b84c79cc89d7d5f9d488cc46f9d
Sean Lin <gshan1209@gmail.com>
2026-08-06 22:26:50 +0800

    feat: export Drive folder topology validator

--- src/cards/drive-assets/index.ts
+++ src/cards/drive-assets/index.ts
@@ -3,6 +3,10 @@ export {
   DRIVE_ASSET_TYPES,
 } from './drive-asset-types.js';
 export { validateDriveAssetRegistry } from './validate-drive-asset-registry.js';
+export {
+  REQUIRED_PHASE1_FOLDER_KEYS,
+  validateDriveFolderRegistry,
+} from './validate-drive-folder-registry.js';
 
 export type {
   DriveAssetRecord,
@@ -23,3 +27,8 @@ export type {
   DriveRegistryIssue,
   DriveRegistryIssueCode,
 } from './validate-drive-asset-registry.js';
+export type {
+  DriveFolderIssue,
+  DriveFolderIssueCode,
+  RequiredPhase1FolderKey,
+} from './validate-drive-folder-registry.js';
